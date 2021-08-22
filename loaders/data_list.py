c28482ff68f398e55cb575e3a7977769b55f6d4c
li.jichang <46667448+lijichang@users.noreply.github.com>
2021-08-22 15:09:17 +0800

    Create data_list.py

---
+++ loaders/data_list.py
@@ -0,0 +1,79 @@
+import numpy as np
+import os
+import os.path
+from PIL import Image
+
+
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def make_dataset_fromlist(image_list):
+    # print("image_list", image_list)
+    with open(image_list) as f:
+        image_index = [x.split(' ')[0] for x in f.readlines()]
+    with open(image_list) as f:
+        label_list = []
+        selected_list = []
+        for ind, x in enumerate(f.readlines()):
+            label = x.split(' ')[1].strip()
+            label_list.append(int(label))
+            selected_list.append(ind)
+        image_index = np.array(image_index)
+        label_list = np.array(label_list)
+    image_index = image_index[selected_list]
+    return image_index, label_list
+
+
+def return_classlist(image_list):
+    with open(image_list) as f:
+        label_list = []
+        for ind, x in enumerate(f.readlines()):
+            label = x.split(' ')[0].split('/')[-2]
+            if label not in label_list:
+                label_list.append(str(label))
+    return label_list
+
+
+class Imagelists_VISDA(object):
+    def __init__(self, image_list, root="../SSDA_MME-master/data/multi/",
+                 transform=None, strong_transform=None, target_transform=None, test=False):
+        imgs, labels = make_dataset_fromlist(image_list)
+        self.imgs = imgs
+        self.labels = labels
+        self.transform = transform
+        self.strong_transform = strong_transform
+        self.target_transform = target_transform
+        self.loader = pil_loader
+        self.root = root
+        self.test = test
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is
+            class_index of the target class.
+        """
+        path = os.path.join(self.root, self.imgs[index])
+        target = self.labels[index]
+        img = self.loader(path)
+        if self.strong_transform  is not None:
+            img_bar = self.strong_transform(img)
+            img_bar2 = self.strong_transform(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        if not self.test:
+            return img, target, index, img_bar, img_bar2
+        else:
+            return img, target, self.imgs[index], index
+
+    def __len__(self):
+        return len(self.imgs)
